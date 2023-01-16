0017d81a2f7d556832b466eff950c03664c45b23
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2023-01-16 16:45:41 +0800

    reset MockPlugin

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## 支持内容 ##
 * mock可以直接开始编写测试文件
 * axios已写好基本的封装
+* 可以自动导入element-plus和vue相关的api
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
-import mockPlugin from './build/vite/plugin';
+import mockPlugin from './build/vite/MockPlugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
