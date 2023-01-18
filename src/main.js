e2d586bd73fb8de8f9d4fd05a63e6fba7c0fd89f
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2023-01-18 10:48:20 +0800

    animate.css

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import api from './api';
 
 import App from './App.vue'
 import router from './router'
+import animate from 'animate.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
     .use(router)
     .use(api)
+    .use(animate)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,4 +5,8 @@ const cnt=ref(0)
 <template>
     <div>{{ cnt }}</div>
     <el-button type="primary" size="default" @click="cnt++">Increment</el-button>
+    <div class="header animate__animated animate__fadeInDown" v-show="cnt%2===0">
+        adsadds
+    </div>
+
 </template>
