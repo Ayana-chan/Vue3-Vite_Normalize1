d3333841b7cb6aef5efcb6319095ddb335bf5d8c
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2023-01-18 11:11:28 +0800

    normalize.css and better Animate.css

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import api from './api';
-
 import App from './App.vue'
 import router from './router'
-import animate from 'animate.css'
+
+import api from './api';
+
+import 'normalize.css';
+import 'animate.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
     .use(router)
     .use(api)
-    .use(animate)
 
 app.mount('#app')
