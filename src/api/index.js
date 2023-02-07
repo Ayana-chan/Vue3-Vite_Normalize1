dbc4bdaede9a5d992632da4f11abbbfb9d7d9aca
Ayana-chan <yychen_114@stu.xidian.edu.cn>
2023-02-07 17:16:15 +0800

    axios自动导出

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,25 @@
-import httpRequest from "@/utils/httpRequest";
+import httpRequest from '@/utils/httpRequest';
 
-//导入各个模块
-import * as baseAxiosTest from "./http/baseAxiosTest";
+//获取所有axios api
+const requireComponent = import.meta.globEager('./http/*.js');
+
+let http = {};
+// 将获取到的文件拆分为'文件名-模块'的键值对并一一加入（module每次都会取出一个文件里面的所有模块）
+Object.entries(requireComponent).forEach(([path, module]) => {
+  // 获取文件名
+  let name = path.substring(path.lastIndexOf('/') + 1);
+  name = name.substring(0, name.lastIndexOf('.'));
+
+  // 添加键值对
+  http[name] = {};
+  // 合并对象，收集模块
+  Object.assign(http[name], module);
+});
+//让httpRequest这一基础封装也加入
+http['httpRequest'] = httpRequest;
 
 export default {
   install: (app) => {
-    app.config.globalProperties['$http'] = {
-      httpRequest,
-      baseAxiosTest,
-
-    };
+    app.config.globalProperties['$http'] = http;
   },
 };
-
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,11 +1,13 @@
 import persist from 'pinia-plugin-persistedstate'; //持久化
-// 导出各个模块
+
+// 获取所有store文件
 const requireComponent = import.meta.globEager('./modules/*.js');
 
 let stores = {};
 // 将获取到的文件拆分为'文件名-模块'的键值对并取出所有模块
-Object.entries(requireComponent).forEach(([k, v]) => {
-  Object.assign(stores, v);
+Object.entries(requireComponent).forEach(([path, module]) => {
+  // 合并对象，收集模块
+  Object.assign(stores, module);
 });
 
 export default {
